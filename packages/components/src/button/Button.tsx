fd997f4baea73d117975d97aa040387e72eb0f42
sidecity.li <sidecity.li@osl.com>
2025-02-12 19:30:07 +0800

    feat: 初始化shadcn

--- packages/components/src/button/Button.tsx
+++ packages/components/src/button/Button.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import './button.css';
 
 export interface ButtonProps {
@@ -31,7 +29,7 @@ export const Button = ({
       style={{ backgroundColor }}
       {...props}
     >
-      {label}55
+      {label}
     </button>
   );
 };
